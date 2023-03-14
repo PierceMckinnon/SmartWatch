49334d2eab89e5e25eb5bb82357ed3c9d783ea2f
pierceMcKinnon <piercemckinnon@hotmail.com>
2023-03-14 16:27:18 -0400

    Change pin config for PCB

--- Include/pinconfig.h
+++ Include/pinconfig.h
@@ -1,6 +1,8 @@
 #ifndef _PIN_CONFIG_H_
 #define _PIN_CONFIG_H_
 
+#ifdef PCB
+
 // Buttons
 #define BUTTONTOPRIGHT 4
 #define BUTTONTOPLEFT 26
@@ -18,3 +20,25 @@
 #define EPAPERBUSY 8
 
 #endif
+
+#ifdef TESTING
+
+// Buttons
+#define BUTTONTOPRIGHT 4
+#define BUTTONTOPLEFT 26
+#define BUTTONBOTTOMLEFT 25
+#define BUTTONBOTTOMRIGHT 10
+
+// General SPI
+#define SPIMOSI 22
+#define SPICLK 24
+
+// Epaper Display
+#define EPAPERRST 12
+#define EPAPERDC 14
+#define EPAPERCS 15
+#define EPAPERBUSY 8
+
+#endif
+
+#endif  // if_PIN_CONFIG_H_
